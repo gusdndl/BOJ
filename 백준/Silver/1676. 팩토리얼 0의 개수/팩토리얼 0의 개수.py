93fa3378b3e1c315162a605c039b736578924d60
Hyun <53088084+gusdndl@users.noreply.github.com>
2023-06-08 00:48:05 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -0,0 +1,15 @@
+#BOJ 1676
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt = 0
+for i in range(1, n+1):
+    if i % 5 == 0:
+        cnt += 1
+    if i % 25 == 0:
+        cnt += 1
+    if i % 125 == 0:
+        cnt += 1
+
+print(cnt)
