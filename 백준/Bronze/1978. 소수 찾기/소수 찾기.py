b9afa8fa878a10b7c9c3ee90297eb3b512f280d3
Hyun <53088084+gusdndl@users.noreply.github.com>
2023-06-08 10:53:16 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,21 @@
+#BOJ 1978
+n = int(input())
+
+def isPrime(n):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, n):
+            if n%i == 0:
+                return False
+        return True
+    
+arr = list(map(int, input().split()))
+
+cnt = 0
+for i in arr:
+
+    if isPrime(i):
+        cnt += 1
+    
+print(cnt)
