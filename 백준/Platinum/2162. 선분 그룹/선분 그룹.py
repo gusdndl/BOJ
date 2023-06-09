c5d3513e68de92f52c009d4091daa691c3f415eb
Hyun <53088084+gusdndl@users.noreply.github.com>
2023-06-09 14:14:29 +0900

    [Platinum V] Title: 선분 그룹, Time: 3628 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Platinum/2162. 선분 그룹/선분 그룹.py
+++ 백준/Platinum/2162. 선분 그룹/선분 그룹.py
@@ -1,86 +1,62 @@
 import sys
-from collections import Counter
 
-input = sys.stdin.readline
+def ccw(p1, p2, p3):
+    return (p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])
 
+def is_intersect(p1, p2, p3, p4):
+    f1 = max(min(p1[0], p2[0]), min(p3[0], p4[0]))
+    f2 = min(max(p1[0], p2[0]), max(p3[0], p4[0]))
+    f3 = max(min(p1[1], p2[1]), min(p3[1], p4[1]))
+    f4 = min(max(p1[1], p2[1]), max(p3[1], p4[1]))
+    a = ccw(p1, p2, p3) * ccw(p1, p2, p4)
+    b = ccw(p3, p4, p1) * ccw(p3, p4, p2)
+    if a == 0 and b == 0:
+        if f1 > f2 or f3 > f4:
+            return False
+    return a <= 0 and b <= 0
 
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+def find(a):
+    if a == parent[a]:
+        return a
+    parent[a] = find(parent[a])
+    return parent[a]
 
-
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
         parent[b] = a
-    else:
-        parent[a] = b
+        group_size[a] += group_size[b]
 
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-class Line:
-    def __init__(self, p1: Point, p2: Point):
-        self.p1 = p1
-        self.p2 = p2
-
-
-def direction(a: Point, b: Point, c: Point):
-    dxab = b.x - a.x
-    dxac = c.x - a.x
-    dyab = b.y - a.y
-    dyac = c.y - a.y
-
-    # AB 기울기 > AC 기울기
-    if dxab * dyac < dyab * dxac:
-        dir = 1
-    # AB 기울기 < AC 기울기
-    elif dxab * dyac > dyab * dxac:
-        dir = -1
-    # AB 기울기 == AC 기울기
-    else:
-        if dxab == 0 and dyab == 0:
-            dir == 0
-        if dxab * dxac < 0 or dyab * dyac < 0:
-            dir = -1
-        elif dxab * dxab + dyab * dyab >= dxac * dxac + dyac * dyac:
-            dir = 0
-        else:
-            dir = 1
-    return dir
-
-
-def intersection(l1: Line, l2: Line):
-    if direction(l1.p1, l1.p2, l2.p1) * direction(l1.p1, l1.p2, l2.p2) <= 0 and \
-            direction(l2.p1, l2.p2, l1.p1) * direction(l2.p1, l2.p2, l1.p2) <= 0:
-        return True
-    return False
-
-
-# N개의 선분
-N = int(input())
-
-lines = []
+N = int(sys.stdin.readline())
 parent = [i for i in range(N)]
+group_size = [1 for _ in range(N)]
+lines = []
+line_data = []
 
 for i in range(N):
-    x1, y1, x2, y2 = map(int, input().rstrip().split())
-    line = Line(Point(x1, y1), Point(x2, y2))
-    lines.append(line)
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    lines.append((x1, y1, x2, y2))
+    line_data.append((min(x1, x2), i))
+    line_data.append((max(x1, x2), i))
 
+line_data.sort()
+
+connected = set()
+for _, idx in line_data:
+    for con in list(connected):
+        if max(lines[con][0], lines[con][2]) < min(lines[idx][0], lines[idx][2]):
+            connected.remove(con)
+        elif is_intersect((lines[idx][0], lines[idx][1]), (lines[idx][2], lines[idx][3]), 
+                          (lines[con][0], lines[con][1]), (lines[con][2], lines[con][3])):
+            union(idx, con)
+    connected.add(idx)
+
+num_groups = set()
+max_size = 0
 for i in range(N):
-    for j in range(i+1, N):
-        if intersection(lines[i], lines[j]):
-            union_parent(parent, i, j)
+    num_groups.add(find(i))
+    max_size = max(max_size, group_size[find(i)])
 
-parent = [find_parent(parent, x) for x in parent]
-
-cnt = Counter(parent)
-print(len(cnt))     # 그룹의 수
-print(max(cnt.values())) 
+print(len(num_groups))
+print(max_size)
