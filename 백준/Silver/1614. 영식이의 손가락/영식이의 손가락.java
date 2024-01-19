2e4b23c5a37879edd64ec425429530bbed99f4a5
Hyun <53088084+gusdndl@users.noreply.github.com>
2024-01-19 20:25:51 +0900

    [Silver III] Title: 영식이의 손가락, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Integer.parseInt(br.readLine());
+		long cnt = Integer.parseInt(br.readLine());
+		long n = N - 1;
+		if (cnt > 0) {
+			if (N % 4 == 1)
+				n += 8 * cnt;
+			else
+				n += cnt / 2 * 8 + cnt % 2 * (8 - (N - 1) * 2);
+		}
+		bw.write(String.valueOf(n));
+		bw.flush();
+	}
+}
