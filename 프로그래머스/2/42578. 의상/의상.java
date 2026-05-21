835ecfa765b2a2fe3bf77bc2e11282ec9f98fbc8
Hyun <53088084+gusdndl@users.noreply.github.com>
2026-05-21 21:53:41 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    static HashMap<String, Integer> map = new HashMap<>();
+
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        for(int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+        for(String str : map.keySet()) {
+            answer *= (map.get(str) + 1);
+        }
+        answer -= 1;
+        return answer;
+    }
+}
