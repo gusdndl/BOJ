755ae0e1bf83ff26f4afe35ccdc4909e50667898
Hyun <53088084+gusdndl@users.noreply.github.com>
2026-05-28 09:41:59 +0900

    [level 2] Title: 롤케이크 자르기, Time: 93.08 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+        
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+        
+        for (int n : topping) {
+            map2.merge(n, 1, Integer::sum);
+        }
+        
+        for (int i = 0; i < topping.length; i++) {
+            Integer num = topping[i];
+            
+            map1.merge(num, 1, Integer::sum);
+            if (map2.get(num) <= 1) {
+                map2.remove(num);
+            } else {
+                map2.merge(num, -1, Integer::sum);
+            }
+            
+            if (map1.size() == map2.size()) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
